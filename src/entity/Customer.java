25512d7f699511ec7656a8a166c96ffcae1467f4
Avrey156 <108403904+Avrey156@users.noreply.github.com>
2024-04-04 23:43:27 +0700

    Create classes in service and utils packages

--- src/entity/Customer.java
+++ src/entity/Customer.java
@@ -1,4 +1,28 @@
 package entity;
 
+import jdk.jfr.Description;
+
 public class Customer {
+    @Description("c - 7 numbers")
+    protected String id;
+    protected String fullName;
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Customer(String id, String fullName) {
+        this.id = id;
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
